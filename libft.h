bb1720d145cf6ac7a18230ece65e9dea4454b486
Thomas Black-Roff <thblack-@c2r2p11.hive.fi>
2025-08-28 15:05:16 +0300

    feature: added strcmp and isnum and added variables for folder paths to makefile

--- libft.h
+++ libft.h
@@ -99,6 +99,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 char	*ft_strdup(const char *s);
